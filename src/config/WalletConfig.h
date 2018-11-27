b15133fe68a21b09058dfb1fc6901d56e2365c08
andy <dirtybitsofficial@gmail.com>
2018-11-26 21:16:24 -0600

    Update WalletConfig.h
    Change "turtle-service" to "catalyst-service"

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -30,7 +30,7 @@ namespace WalletConfig
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
-    const std::string walletdName = "turtle-service";
+    const std::string walletdName = "catalyst-service";
 
     /* The full name of your crypto */
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
