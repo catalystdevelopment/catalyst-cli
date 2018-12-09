517c8010b22aa86f7e8806ff429d26c6e6b13adb
andy <dirtybitsofficial@gmail.com>
2018-12-08 19:22:48 -0600

    Update CryptoNoteProtocolHandler.cpp
    change colors, network synchronization message

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -605,7 +605,7 @@ bool CryptoNoteProtocolHandler::request_missing_objects(CryptoNoteConnectionCont
     requestMissingPoolTransactions(context);
 
     context.m_state = CryptoNoteConnectionContext::state_normal;
-    logger(Logging::INFO, Logging::BRIGHT_GREEN) << context << "Successfully synchronized with the TurtleCoin Network.";
+    logger(Logging::INFO, Logging::BRIGHT_BLUE) << context << "Successfully synchronized with the Catalyst Network.";
     on_connection_synchronized();
   }
   return true;
@@ -623,7 +623,7 @@ bool CryptoNoteProtocolHandler::on_connection_synchronized() {
       logger(INFO, WHITE) << " If you need more assistance, you can contact us for support at " + WalletConfig::contactLink << ENDL;
       logger(INFO, BRIGHT_MAGENTA) << "===================================================" << ENDL << ENDL ;
 
-      logger(INFO, BRIGHT_GREEN) << asciiArt << ENDL;
+      logger(INFO, BRIGHT_BLUE << asciiArt << ENDL;
 
     m_observerManager.notify(&ICryptoNoteProtocolObserver::blockchainSynchronized, m_core.getTopBlockIndex());
   }
