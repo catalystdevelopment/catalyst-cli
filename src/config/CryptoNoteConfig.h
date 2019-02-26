cf0ecf8d51ef367a73da46041da5f441e774f9b4
dirtybits <dirtybitsofficial@gmail.com>
2019-02-26 00:55:40 -0600

    Merge remote-tracking branch 'upstream/development' into development
    # Conflicts:
    #	CMakeLists.txt
    #	README.md
    #	src/CMakeLists.txt
    #	src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
    #	src/config/CryptoNoteCheckpoints.h
    #	src/config/CryptoNoteConfig.h
    #	src/config/WalletConfig.h
    #	src/config/version.h.in
    #	src/zedwallet++/CommandImplementations.cpp

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -137,6 +137,7 @@ const uint32_t UPGRADE_HEIGHT_V2                             = 1;
 const uint32_t UPGRADE_HEIGHT_V3                             = 2;
 const uint32_t UPGRADE_HEIGHT_V4                             = 3; // Upgrade height for CN-Lite Variant 1 switch.
 const uint32_t UPGRADE_HEIGHT_CURRENT                        = UPGRADE_HEIGHT_V4;
+const uint32_t UPGRADE_HEIGHT_V5                             = 700000;
 
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
