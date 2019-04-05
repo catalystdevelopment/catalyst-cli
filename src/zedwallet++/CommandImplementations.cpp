8a938720f9675948498a4c113f135268dd51ae9b
Brandon Lehmann <brandonlehmann@gmail.com>
2019-04-04 23:52:07 -0400

    Restore erroneously removed copyright header during previous rebase

--- src/zedwallet++/CommandImplementations.cpp
+++ src/zedwallet++/CommandImplementations.cpp
@@ -1,3 +1,4 @@
+// Portions Copyright (c) 2018-2019, The Catalyst Developers
 // Copyright (c) 2018-2019, The TurtleCoin Developers
 // 
 // Please see the included LICENSE file for more information.
